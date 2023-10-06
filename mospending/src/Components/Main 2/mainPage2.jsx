572c151b4174d258c7e1d03406a7b16d1dfb1147
tomide <tomideadeyanju@gmail.com>
2023-10-07 00:52:00 +0100

    Added new components

--- mospending/src/Components/Main 2/mainPage2.jsx
+++ mospending/src/Components/Main 2/mainPage2.jsx
@@ -2,6 +2,10 @@ import React from "react";
 import Frame1 from "../../assets/Frame1.png";
 import Frame2 from "../../assets/Frame2.png";
 import Hand from "../../assets/Hand.png";
+import Money from "../../assets/money.png";
+import People from "../../assets/People.png";
+import Card from "../../assets/Card.png";
+import Atm from "../../assets/Atmhand.png";
 import { BiSolidCheckCircle } from "react-icons/bi";
 const MainPage2 = () => {
   return (
@@ -27,16 +31,15 @@ const MainPage2 = () => {
           <p className="text-3xl font-extrabold bolderText  text-slate-900">
             Instant Visa Card
           </p>
-          <p className="breakText mt-[20px] text-slate-400 introduce font-extralight">
+          <p className="breakText mt-[20px] text-slate-400 introduce font-light">
             Introducing our new instant Visa card feature! With just a few taps
-            in  our app, you can instantly create a virtual Visa card and
-            start making  purchases online or in-store. Say goodbye to
-            waiting for your physical
-            card to arrive in the mail and hello to instant spending
-            power. Try it
-            today and experience the convenience of our fintech app.{" "}
+            in our app, you can instantly create a virtual Visa card and start
+            making purchases online or in-store. Say goodbye to waiting for your
+            physical card to arrive in the mail and hello to instant spending
+            power. Try it today and experience the convenience of our fintech
+            app.{" "}
           </p>
-          <div className="gap-[20px]  smallerText font-extralight">
+          <div className="gap-[20px]  smallerText font-light">
             <p className="flex gap-4 text-slate-400">
               {" "}
               <BiSolidCheckCircle className="text-orange-500" />
@@ -53,6 +56,60 @@ const MainPage2 = () => {
               Flexibility
             </p>
           </div>
+        </div>
+      </div>
+
+      <div>
+        <div className="flex justify-around flexSmall">
+          <div className="p-[50px] flex justify-center align-center flex-col flexX ">
+            <p className="text-4xl font-extrabold Xbold">Easy Tracking</p>
+            <p className="font-light">
+              Easily track your payments with our payment tracking feature.
+              Monitor your bills and payments in one place, and get alerts when
+              bills are due, so you never miss a payment again. With our app,
+              you can simplify your finances and stay organized.
+            </p>
+          </div>
+          <img src={People} className="flexImg" />
+        </div>
+      </div>
+
+      <div className=" w-100 h-[100vh]">
+        <p className="text-center text-3xl">We have more offers for you</p>
+        <p className="text-center font-light">
+          With features like customizable dashboards, real-time alerts, and
+          secure data encryption, <br /> you can effortlessly manage your
+          transactions and take control of your financial life.
+        </p>
+
+        <div className="flex justify-around">
+          <div className="bg-orange-400 p-[20px]  mt-[30px] h-[60vh] rounded-[10px] w-[30vw]">
+            <p className="text-2xl">
+              Send and recieve without <br /> border
+            </p>
+            <p className="text-blue-700 font-light">Explore Payments </p>
+            <div className="flex justify-center align-center">
+              <img className="w-[300px]" src={Money} />
+            </div>
+          </div>
+
+          <div className="bg-blue-800 p-[20px] mt-[30px] h-[60vh] rounded-[10px] w-[30vw]">
+          <p className="text-white text-2xl">Invoice that works smarter</p>
+          <p className="text-orange-300">Learn more about invoices</p>
+          <div className="flex justify-center align-center mt-[50px]">
+              <img className="w-[300px]" src={Card} />
+            </div>
+          </div>
+        </div>
+
+
+        <div className=" w-100 h-[100vh] mt-[20px] flex align-center justify-center">
+          <div className='bg-green-700 p-[20px] w-[50vw] rounded-[10px] mt-[20px] round-[10px] h-[50vh]'>
+          <p className="text-slate-200 text-2xl ">Smart company debit cards help you <br /> and team go further </p>
+          <p className="text-orange-200 font-light">Learn more about Cards</p>
+          <img src={Atm} />
+          </div>
+         
         </div>
       </div>
     </div>
