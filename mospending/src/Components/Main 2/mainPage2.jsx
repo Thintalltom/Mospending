f3864ef4ddd2d1d881c6df31c70445bd2ba252be
tomide <tomideadeyanju@gmail.com>
2023-10-09 21:40:19 +0100

    changes made added more components

--- mospending/src/Components/Main 2/mainPage2.jsx
+++ mospending/src/Components/Main 2/mainPage2.jsx
@@ -3,7 +3,7 @@ import Frame1 from "../../assets/Frame1.png";
 import Frame2 from "../../assets/Frame2.png";
 import Hand from "../../assets/Hand.png";
 import Money from "../../assets/money.png";
-import People from "../../assets/People.png";
+import People from "../../assets/people.png";
 import Card from "../../assets/Card.png";
 import Atm from "../../assets/Atmhand.png";
 import Cone from "../../assets/Cone.png";
@@ -11,6 +11,7 @@ import Vector from "../../assets/Vector.png";
 import Orange from "../../assets/47orange.png";
 import Logo from "../../assets/logo.png";
 import { BiSolidCheckCircle } from "react-icons/bi";
+import { BsTwitter, BsLinkedin, BsFacebook, BsInstagram } from "react-icons/bs";
 const MainPage2 = () => {
   return (
     <div className="mt-[450px]  w-full h-screen">
@@ -78,94 +79,124 @@ const MainPage2 = () => {
         </div>
       </div>
 
-      <div className=" w-100 h-[100vh]">
-        <p className="text-center text-3xl">We have more offers for you</p>
-        <p className="text-center font-light">
+      {/*the Box section */}
+      <div className=" w-100 h-[100vh] boxOffers">
+        <p className="text-center text-3xl offers">
+          We have more offers for you
+        </p>
+        <p className="text-center font-light offersText">
           With features like customizable dashboards, real-time alerts, and
           secure data encryption, <br /> you can effortlessly manage your
           transactions and take control of your financial life.
         </p>
 
-        <div className="flex justify-around">
-          <div className="bg-orange-400 p-[20px]  mt-[30px] h-[60vh] rounded-[10px] w-[30vw]">
+        <div className="flex justify-around sendBox">
+          <div className="bg-orange-400 p-[20px] sendBox1  mt-[30px] h-[60vh] rounded-[10px] w-[30vw]">
             <p className="text-2xl">
               Send and recieve without <br /> border
             </p>
             <p className="text-blue-700 font-light">Explore Payments </p>
             <div className="flex justify-center align-center">
-              <img className="w-[300px]" src={Money} />
+              <img className="w-[300px] moneyBox" src={Money} />
             </div>
           </div>
 
-          <div className="bg-blue-800 p-[20px] mt-[30px] h-[60vh] rounded-[10px] w-[30vw]">
+          <div className="bg-blue-800 p-[20px] mt-[30px] h-[60vh] sendBox1 rounded-[10px] w-[30vw]">
             <p className="text-white text-2xl">Invoice that works smarter</p>
             <p className="text-orange-300">Learn more about invoices</p>
             <div className="flex justify-center align-center mt-[50px]">
-              <img className="w-[300px]" src={Card} />
+              <img className="w-[300px] moneyBox" src={Card} />
             </div>
           </div>
         </div>
 
-        <div className="  mt-[20px] flex align-center justify-center">
-          <div className="bg-green-700 p-[20px] w-[80vw] h-[50vh]  rounded-[10px] mt-[20px] round-[10px]">
+        {/*the Smart Company section */}
+        <div className="  mt-[20px] flex align-center justify-center smart">
+          <div className="bg-green-700 p-[20px] smartHold w-[80vw] h-[50vh]  rounded-[10px] mt-[20px] round-[10px]">
             <p className="text-slate-200 text-2xl ">
               Smart company debit cards help you <br /> and team go further{" "}
             </p>
             <p className="text-orange-200 font-light">Learn more about Cards</p>
-            <img src={Atm} className="w-[450px] float-right mt-[-109px]" />
+            <img
+              src={Atm}
+              className="w-[450px] float-right mt-[-109px] smartAtm"
+            />
           </div>
         </div>
 
-        <div className="flex justify-center align-center mt-[50px]">
-          <div className="bg-[#3B38B2] w-[80vw] h-[50vh] rounded-[20px]">
-            <img src={Cone} />
-            <div className="flex justify-center align-center">
-              <img src={Vector} className="w-[100px]" />
+        {/*the Suggestion section */}
+        <div className="flex justify-center align-center mt-[50px] suggest">
+          <div className="bg-[#3B38B2] w-[80vw] h-[50vh] suggestBox1 rounded-[20px]">
+            <img src={Cone} className="suggestBox1Img" />
+            <div className="flex justify-center suggestBox2 align-center">
+              <img src={Vector} className="w-[100px] suggestBox2Img" />
               <p className="text-6xl text-center text-white">
                 We offer personalised <br /> saving suggestions
               </p>
             </div>
-            <div className="flex justify-around">
-              <img src={Orange} />
-              <button className="text-white p-[20px] h-[60px] mt-[20px] bg-[#FFA051] rounded-[30px]">
+            <div className="flex justify-around orangeBox">
+              <img src={Orange} className="orange1" />
+              <button className="text-white p-[20px] orangeBtn h-[60px] mt-[20px] bg-[#FFA051] rounded-[30px]">
                 {" "}
                 Create Account
               </button>
-              <img src={Orange} />
+              <img src={Orange} className="orange1" />
             </div>
           </div>
         </div>
 
         {/*the footer section */}
-        <div className="bg-slate-900 mt-[-70px] h-[50vh] flex ">
-
-          <div className="h-[50vh] w-screen">
-            <div className=" h-[30vh] mt-[150px] p-[20px]">
-              <div className="flex justify-between">
+        <div className="bg-[#000000] mt-[-70px] h-[50vh] footerS0 flex ">
+          <div className="h-[50vh] w-screen footerS1">
+            <div className=" h-[30vh] mt-[150px] p-[20px] footerS2">
+              <div className="flex justify-between footerS3">
                 <div>
-                 <img src={Logo} />
-            <p>Discover the power of fintech</p>
-                </div>
-           
-            <div className="flex justify-between gap-[80px] p-4 text-white">
-              <div>
-              <p>Product</p>
-              </div>
-              <div>
-                <p>Company</p>
-                </div>
-                <div>
-                <p>Utility</p>
-                </div>
-             
+                  <img src={Logo} />
+                  <p className="text-white font-light ">
+                    Discover the power of fintech
+                  </p>
 
-            </div>
+                  <div className="flex gap-9 mt-[10px] text-white">
+                    <BsFacebook />
+                    <BsInstagram />
+                    <BsTwitter />
+                    <BsLinkedin />
+                  </div>
+                </div>
+
+                <div className="flex hover:cursor-pointer footerS4 justify-between gap-[80px] p-4 text-white">
+                  <div className="p0">
+                    <p className="font-light">Product</p>
+                    <div className="mt-4 p1">
+                      <p className="font-light">How it works </p>
+                      <p className="font-light">Pricing </p>
+                    </div>
+                  </div>
+                  <div className="text-white p1">
+                    <p className="font-light">Company</p>
+                    <div className="mt-4">
+                      <p className="font-light">About </p>
+                      <p className="font-light">FAQs</p>
+                      <p className="font-light">Contact</p>
+                      <p className="font-light">Blog</p>
+                    </div>
+                  </div>
+                  <div className="text-white p2">
+                    <p className="font-light">Utility</p>
+                    <div className="mt-4">
+                      <p className="font-light">Privacy Policy</p>
+                      <p className="font-light">Term of use </p>
+                      <p className="font-light">404 page</p>
+                    </div>
+                  </div>
+                </div>
               </div>
-            
             </div>
-     
+            <hr className="bg-red-500  mt-[-30px]" />
+            <p className="text-white font-light text-xs mt-4">
+              &copy; Mospending Poweredby SilverGeorge
+            </p>
           </div>
-
         </div>
       </div>
     </div>
